0f13353518297ddb7bbf2bb2cfdb8fbbaa5bc2ea
garanaurt <bnotchenko@gmail.com>
2024-02-15 10:06:46 +0200

    deploy

--- new/user_data.py
+++ new/user_data.py
@@ -1,21 +1,21 @@
 
 
-ADMIN_ID = 1103549193  #5353394478 #
+ADMIN_ID = 1103549193  
 
 #from BotFather - бот которого нужно добавить на все ваши каналы - он будет выкладывать туда посты
-TOKEN = "6666493087:AAEtWZMD_wkHYsVC9NbsGzTIpfl274g0HEI"#"6494104145:AAEnz3VnAN9HTYQpQcfdd0jzl8m_yfCAujI" #
-USERNAME = "@omgsacred_bot"#'@main_admin_multiresender_bot'#
+TOKEN = "6666493087:AAEtWZMD_wkHYsVC9NbsGzTIpfl274g0HEI"
+USERNAME = "@omgsacred_bot"
 
 
 
 
 #from https://my.telegram.org/auth - нужно зайти в аккаунт заполнить данные и получить хеш и айди
-API_HASH = 'c869e4f165b45620c119899ae1fa3066' #246593ffb133a9da5edf8d9246708855'
-API_ID = '27126179'                           #- arag....  bo - '21919939'
-API_NUM = '380934998149'
-#Этим же аккаунтом написать боту https://t.me/getmyid_bot и вписать ниже айди этому же боту можно 
+API_HASH = 'c869e4f165b45620c1198ae1fa3066' 
+API_ID = '271179'                           
+API_NUM = '38098149'
+#Этим же аккаунтом написать боту https://t.me/getmyid_bot и вписать ниже айди. Этому же боту можно 
 #присылать посты из каналов и он будет показывать их айди
-USERBOT_USER_ID = 5703939637
+USERBOT_USER_ID = 57039337
 
 
 db_path = 'data.db'
